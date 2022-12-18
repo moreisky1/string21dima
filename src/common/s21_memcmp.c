61c17646a99c3732d66eff996b0dec6f538c1274
barbreye <barbreye@student.21-school.ru>
2022-12-18 10:05:42 +0300

    barbreye --> memcmp --> OK

--- src/common/s21_memcmp.c
+++ src/common/s21_memcmp.c
@@ -1,1 +1,18 @@
 #include "../s21_string.h"
+
+int s21_memcmp(const void *str1, const void *str2, size_t n) {
+    int result = 0;
+    int i = 0;
+    char* st1 = (char*) str1;
+    char* st2 = (char*) str2;
+
+    while (i < n) {
+        if (st1[i] != st2[i]) {
+            break;
+        }
+        i++;
+    }
+    if (st1[i] - st2[i] > 0) result = 1;
+    if (st1[i] - st2[i] < 0) result = (-1);
+    return result;
+}
