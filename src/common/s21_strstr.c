f5cdafb5fbdf1b9e68a2c79dcfe12e6d5fd7dad6
Bottle Impulse <bottleim@ox-r6.msk.21-school.ru>
2022-12-21 16:40:27 +0300

    barbreye_strstr --> bottleim --> developing

--- src/common/s21_strstr.c
+++ src/common/s21_strstr.c
@@ -1,30 +1,30 @@
 #include "../s21_string.h"
 
 char* s21_strstr(const char *haystack, const char *needle) {
-    char* result = NULL;
-    int i = 0, j = 0;
-    int marker = 0;
-    if(haystack != NULL && needle != NULL){
-        while (haystack[i]!='\0') {
-        j = 0;
-        while (needle[j] != '\0') {
-            if(needle[j] != haystack[i + j]) {
-                break;
-            }
-            if(haystack[i + j] == '\0') {
-                break;
-            }
-            j++;
+  char* result = NULL;
+  int i = 0, j = 0;
+  int marker = 0;
+  if(haystack != NULL && needle != NULL){
+    while (haystack[i]!='\0') {
+      j = 0;
+      while (needle[j] != '\0') {
+        if(needle[j] != haystack[i + j]) {
+          break;
         }
-        if(needle[j] == '\0') {
-            marker = 1;
-            break;
+        if(haystack[i + j] == '\0') {
+          break;
         }
-        i++;
+          j++;
+      }
+      if(needle[j] == '\0') {
+        marker = 1;
+        break;
+      }
+      i++;
     }
-        if (marker != 0) {
-            result = (char *)haystack + i;
-        }
+    if (marker != 0) {
+      result = (char *)haystack + i;
     }
-    return result;
+  }
+  return result;
 }
--- src/tests/s21_strstr_test/s21_strstr_test.c
+++ src/tests/s21_strstr_test/s21_strstr_test.c
@@ -3,8 +3,8 @@
 START_TEST(s21_strstr_1) {
         char str[] = "HHelloHelsdfgsbsb";
         char str2[] = "sd";
-        char* s21 = s21_strstr(str,str2);
-        char* or = strstr(str,str2);
+        // char* s21 = s21_strstr(str,str2);
+        // char* or = strstr(str,str2);
         ck_assert_str_eq(s21_strstr(str,str2),strstr(str,str2));
 
 } END_TEST
@@ -12,8 +12,8 @@ START_TEST(s21_strstr_1) {
 START_TEST(s21_strstr_2) {
     char str[] = "nulllll";
     char str2[] = "u";
-    char* s21 = s21_strstr(str,str2);
-    char* or = strstr(str,str2);
+    // char* s21 = s21_strstr(str,str2);
+    // char* or = strstr(str,str2);
     ck_assert_str_eq(s21_strstr(str,str2),strstr(str,str2));
 
 } END_TEST
@@ -21,8 +21,8 @@ START_TEST(s21_strstr_2) {
 START_TEST(s21_strstr_3) {
     char str[] = "Hello";
     char str2[] = "Hello";
-    char* s21 = s21_strstr(str,str2);
-    char* or = strstr(str,str2);
+    // char* s21 = s21_strstr(str,str2);
+    // char* or = strstr(str,str2);
     ck_assert_str_eq(s21_strstr(str,str2),strstr(str,str2));
 
 } END_TEST
@@ -30,8 +30,8 @@ START_TEST(s21_strstr_3) {
 START_TEST(s21_strstr_4) {
     char str[] = "Hello\n";
     char str2[] = "ll";
-    char* s21 = s21_strstr(str,str2);
-    char* or = strstr(str,str2);
+    // char* s21 = s21_strstr(str,str2);
+    // char* or = strstr(str,str2);
     ck_assert_str_eq(s21_strstr(str,str2),strstr(str,str2));
 
 } END_TEST
@@ -39,8 +39,8 @@ START_TEST(s21_strstr_4) {
 START_TEST(s21_strstr_5) {
     char str[] = "School21";
     char str2[] = "21";
-    char* s21 = s21_strstr(str,str2);
-    char* or = strstr(str,str2);
+    // char* s21 = s21_strstr(str,str2);
+    // char* or = strstr(str,str2);
     ck_assert_str_eq(s21_strstr(str,str2),strstr(str,str2));
 
 } END_TEST
@@ -48,8 +48,8 @@ START_TEST(s21_strstr_5) {
 START_TEST(s21_strstr_6) {
     char str[] = "Fooo";
     char str2[] = "o";
-    char* s21 = s21_strstr(str,str2);
-    char* or = strstr(str,str2);
+    // char* s21 = s21_strstr(str,str2);
+    // char* or = strstr(str,str2);
     ck_assert_str_eq(s21_strstr(str,str2),strstr(str,str2));
 
 } END_TEST
@@ -57,8 +57,8 @@ START_TEST(s21_strstr_6) {
 START_TEST(s21_strstr_7) {
     char str[] = "aaaaaaaaaaa";
     char str2[] = "a";
-    char* s21 = s21_strstr(str,str2);
-    char* or = strstr(str,str2);
+    // char* s21 = s21_strstr(str,str2);
+    // char* or = strstr(str,str2);
     ck_assert_str_eq(s21_strstr(str,str2),strstr(str,str2));
 
 } END_TEST
@@ -87,8 +87,8 @@ START_TEST(s21_strstr_9) {
 START_TEST(s21_strstr_10) {
     char str[] = "fdkglks";
     char str2[] = "s";
-    char* s21 = s21_strstr(str,str2);
-    char* or = strstr(str,str2);
+    // char* s21 = s21_strstr(str,str2);
+    // char* or = strstr(str,str2);
     ck_assert_str_eq(s21_strstr(str,str2),strstr(str,str2));
 
 } END_TEST
@@ -99,6 +99,21 @@ START_TEST(s21_strstr_11) {
 
  ck_assert_ptr_null(s21_strstr(str,str2));
  //Опция отличается от оригинального strstr. Оригинальный не обрабатывает значение ноль и рубит с ошибкой
+
+} END_TEST
+
+START_TEST(s21_strstr_12) {
+//   char str[] = "fdkglks";
+//   char str2[] = "s";
+  ck_assert_str_eq(s21_strstr("\0","\0"),strstr("\0","\0"));
+} END_TEST
+
+START_TEST(s21_strstr_13) {
+    char str[] = "Hellow my frends";
+    char str2[] = "dsa";
+    // char* s21 = s21_strstr(str,str2);
+    // char* or = strstr(str,str2);
+    ck_assert_str_eq(s21_strstr(str,str2),strstr(str,str2));
 
 } END_TEST
 
@@ -117,6 +132,8 @@ Suite* s21_strstr_create_suite(void) {
     tcase_add_test(tc,s21_strstr_9);
     tcase_add_test(tc,s21_strstr_10);
     tcase_add_test(tc,s21_strstr_11);
+    tcase_add_test(tc,s21_strstr_12);
+    tcase_add_test(tc,s21_strstr_13);
     suite_add_tcase(s,tc);
     return s;
 }
