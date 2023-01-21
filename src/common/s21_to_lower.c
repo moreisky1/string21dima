1ac93f8b849e7505200d3786cdae9085bce7c65e
Alejandro Lyndsay <alejandr@ox-t5.msk.21-school.ru>
2023-01-21 18:54:02 +0300

    alejandr --> developing

--- src/common/s21_to_lower.c
+++ src/common/s21_to_lower.c
@@ -1,18 +1,21 @@
 #include "../s21_string.h"
 
-void* s21_to_lower(const char *str) {
-    int i = 0;
-    char* result = NULL;
-    if (str != NULL) {
-        result = (char *) malloc((s21_strlen(str) + 1) * sizeof(char));
-        while (str[i] != '\0') {
-            result[i] = str[i];
-            if (result[i] >= 'A' && result[i] <= 'Z') {
-                result[i] = result[i] + 32;
-            }
-            i++;
-        }
-        result[i] = '\0';
+void* s21_to_lower(const char* str) {
+  char* result;
+  size_t i = 0;
+  if (str == NULL) {
+    result = NULL;
+  } else {
+    result = malloc(((s21_strlen(str) + 1) * sizeof(char)));
+    while (str[i] != '\0') {
+      if (str[i] >= 65 && str[i] <= 90) {
+        result[i] = str[i] + 32;
+      } else {
+        result[i] = str[i];
+      }
+      i++;
     }
-    return result;
+    result[i] = '\0';
+  }
+  return result;
 }
