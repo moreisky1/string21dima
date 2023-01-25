aa5f9739416d35964227399160f4eac03be19aa4
Alejandro Lyndsay <alejandr@il-i3.msk.21-school.ru>
2023-01-25 16:09:54 +0300

    alejandr -> developing

--- src/common/s21_to_upper.c
+++ src/common/s21_to_upper.c
@@ -1,18 +1,21 @@
 #include "../s21_string.h"
 
-void* s21_to_upper(const char *str) {
-    char* result = NULL;
-    if (str != NULL) {
-     int i = 0;
-        result = (char *) malloc((s21_strlen(str) + 1) * sizeof (char ));
-        while(str[i] != '\0') {
-         result[i] = str[i];
-         if(result[i] >= 'a' && result[i] <= 'z') {
-             result[i] = result[i] - 32;
-         }
-         i++;
-        }
-        result[i] = '\0';
+void* s21_to_lower(const char* str) {
+  char* result;
+  size_t i = 0;
+  if (str == NULL) {
+    result = NULL;
+  } else {
+    result = malloc(((s21_strlen(str) + 1) * sizeof(char)));
+    while (str[i] != '\0') {
+      if (str[i] >= 97 && str[i] <= 122) {
+        result[i] = str[i] - 32;
+      } else {
+        result[i] = str[i];
+      }
+      i++;
     }
-    return result;
+    result[i] = '\0';
+  }
+  return result;
 }
