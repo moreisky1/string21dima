cbd5e7fc612d14cf78d2e45c4d2d1fe43d718a69
bottleim <bottleim@student.21-school.ru>
2023-01-27 15:10:47 +0300

    barbreye_trim --> bottleim --> developing

--- src/common/s21_trim.c
+++ src/common/s21_trim.c
@@ -2,18 +2,14 @@
 
 int contain_list(char c, const char *list);
 size_t s21_strespn(const char *str1, const char *str2);
+char * whiteSpase(char * src, char * trim_chars, int flag);
 
 void* s21_trim(const char *src, const char *trim_chars) {
     char* result = NULL;
     if (src == NULL && trim_chars == NULL) {
         result = NULL;
     } else if (trim_chars == NULL) {
-        size_t size = s21_strlen(src);
-        result = (char *) malloc((size + 1) * sizeof(char));
-        char whitespase[] = {9, 10, 11, 12, 13, 32, '\0'};
-        size_t start_step = s21_strspn(src, whitespase);
-        size_t end_step = s21_strespn(src + start_step, whitespase);
-        s21_strncpy(result, src + start_step, size - start_step - end_step);
+        result = whiteSpase(src, trim_chars, 1);
         // int j = 0;
         // while (src[j] != '\0') {
         //     result[j] = src[j];
@@ -21,12 +17,13 @@ void* s21_trim(const char *src, const char *trim_chars) {
         // }
         // result[j] = '\0';
     } else if (src != NULL) {
-        size_t size = s21_strlen(src);
-        result = (char *) malloc((size + 1) * sizeof(char));
-        // char * whitespase = {9, 10, 11, 12, 13, 32, '\0'};
-        size_t start_step = s21_strspn(src, trim_chars);
-        size_t end_step = s21_strespn(src + start_step, trim_chars);
-        s21_strncpy(result, src + start_step, size - start_step - end_step);
+        if (!s21_strlen(trim_chars)) {
+            result = whiteSpase(src, trim_chars, 1);
+        } else {
+            result = whiteSpase(src, trim_chars, 0);
+        }
+        
+        
         // int start = 0, end = 0, i = s21_strlen(src) - 1, len = 0;
         // // check start
         // while (contain_list(src[start], trim_chars)) {
@@ -60,6 +57,24 @@ void* s21_trim(const char *src, const char *trim_chars) {
     }
     return result;
 }
+
+char * whiteSpase(char * src, char * trim_chars, int flag) {
+    size_t size = s21_strlen(src), start_step = 0, end_step = 0;
+    char * result = (char *) malloc((size + 1) * sizeof(char));
+    if (flag) {
+        char whitespase[] = {9, 10, 11, 12, 13, 32, '\0'};
+        start_step = s21_strspn(src, whitespase);
+        end_step = s21_strespn(src + start_step, whitespase);
+        s21_strncpy(result, src + start_step, size - start_step - end_step);
+    } else {
+        start_step = s21_strspn(src, trim_chars);
+        end_step = s21_strespn(src + start_step, trim_chars);
+        s21_strncpy(result, src + start_step, size - start_step - end_step);
+    }
+    result[size - start_step - end_step] = 0;
+    return result;
+}
+
 
 int contain_list(char c, const char *list) {
     int result = 0;
