5c946aab1fc94ce01a8e36a20e91471fe282e3dd
Alejandro Lyndsay <alejandr@il-i2.msk.21-school.ru>
2023-01-29 15:18:05 +0300

    develop_alejandr -> alejandr -> strtok_done

--- src/common/s21_strtok.c
+++ src/common/s21_strtok.c
@@ -36,29 +36,37 @@ char* s21_strtok(char* str, const char* delim) {
   } else if (str == NULL) {  // либо вывод новых лексем, либо выход
     if (flag != 0) {  // новые лексемы
       if (delim != NULL) {
-        int flag_start = 0;
-        int flag_break = 0;
-        for (int i = 0; i < s21_strlen(pointer); i++) {
-          for (int j = 0; j < s21_strlen(delim); j++) {
-            if (pointer[i] != delim[j]) {
-              flag_start = 1;
+        if (s21_strlen(pointer) == 0) {
+          mem = NULL;
+        } else {
+          int flag_start = 0;
+          int flag_break = 0;
+          for (int i = 0; i < s21_strlen(pointer); i++) {
+            for (int j = 0; j < s21_strlen(delim); j++) {
+              if (pointer[i] != delim[j]) {
+                flag_start = 1;
+              }
+              if ((pointer[i] == delim[j]) && flag_start == 0) {
+                pointer++;
+                continue;
+              } else if ((pointer[i] == delim[j]) && flag_start != 0) {
+                pointer[i] = '\0';
+                mem = pointer;
+                pointer = pointer + i + 1;
+                flag_break = 1;
+                break;
+              }
+              if (pointer[i + 1] == '\0') {
+                mem = pointer;
+                pointer = pointer + i + 1;
+              }
             }
-            if ((pointer[i] == delim[j]) && flag_start == 0) {
-              pointer++;
-              continue;
-            } else if ((pointer[i] == delim[j]) && flag_start != 0) {
-              pointer[i] = '\0';
-              mem = pointer;
-              pointer = pointer + i + 1;
-              flag_break = 1;
+            if (flag_break == 1) {
               break;
             }
           }
-          if (flag_break == 1) {
-            break;
-          }
         }
-      } else if (delim == NULL) {
+      } else if (delim == NULL) {  //
         mem = NULL;
       }
     } else {  // выход
