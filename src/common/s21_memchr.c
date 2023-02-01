5053b718253efc9916e88246083ec151bbbc56df
alexandr <alexandr@Ubuntu.myguest.virtualbox.org>
2023-02-01 20:14:10 +0300

    новая реализация, текущие замечания устранил

--- src/common/s21_memchr.c
+++ src/common/s21_memchr.c
@@ -1,40 +1,17 @@
 #include "../s21_string.h"
 
-int contain(const char *str, int c, size_t n) ;
-
-void* s21_memchr(const void *str, int c, size_t n) {
-    void* result = NULL;
-    char* src = (char*)str;
+void *s21_memchr(const void *str, int c, size_t n) {
+    unsigned char *buf = (unsigned char *)str;
     size_t i = 0;
-    while(i < n) {
+    char *ans = NULL;
+    while (i < n && buf) {
+        if (*buf == (unsigned char)c) {
+            ans = buf;
+            i = n;
+        }
+        buf++;
         i++;
-        if (src[i] == c) {
-            break;
-        }
-        if (src[i] == '\0') {
-            break;
-        }
     }
-    if(contain((char*)str,c,n) == 1) {
-        result = (void*)(str + i);
-    }
-    return result;
-}
-
-int contain(const char *str, int c, size_t n) {
-    int res = 0;
-    size_t i = 0;
-    while(i < n) {
-        i++;
-        if (str[i] == c) {
-            res = 1;
-            break;
-        }
-        if (str[i] == '\0') {
-            break;
-
-        }
-    }
-    return res;
+    return ans;
 }
 
